f925e149cae33eca0b577395349fcd68f6dce898
MicaellyUchoa <micaellyit@hotmail.com>
2020-08-07 18:08:45 -0300

    feature: implementando api de connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image, Text } from "react-native";
 import styles from "./styles";
 import landingImg from "../../assets/images/landing.png";
@@ -7,8 +7,17 @@ import giveClassesImg from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+import api from "../../services/api";
 function Landing() {
     const { navigate } = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get("/connections").then(response => {
+            const { total } = response.data;
+            setTotalConnections(total);
+        });
+    }, []);
 
     function handleNavigateToPage(page: string) {
         navigate(page);
@@ -17,7 +26,7 @@ function Landing() {
     function handleNavigateToStudyPage() {
         navigate("Study");
     }
-    
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner} />
@@ -41,7 +50,8 @@ function Landing() {
                 </RectButton>
             </View>
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas <Image source={heartIcon} />
+                Total de {totalConnections} conexões já realizadas{" "}
+                <Image source={heartIcon} />
             </Text>
         </View>
     );
