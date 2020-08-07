c2065c1fb9ec7132d733a9126d4115aaa2972f3d
MicaellyUS <mus@topnode.com.br>
2020-08-06 22:49:47 -0300

    feature: adicionando navigation ao app.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import Landing from "./src/pages/Landing";
+import { AppLoading } from "expo";
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from "@expo-google-fonts/archivo";
+import { Poppins_400Regular, Poppins_600SemiBold } from "@expo-google-fonts/poppins";
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+        Archivo_400Regular,
+        Archivo_700Bold,
+        Poppins_400Regular,
+        Poppins_600SemiBold,
+    });
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+    return (
+        <>
+            <StatusBar style="light" />
+            <AppStack />
+        </>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ff11f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
