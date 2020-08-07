7791ed5e6233705a0bd0e53f944fdc8f2b8c6d60
MicaellyUchoa <micaellyit@hotmail.com>
2020-08-07 17:34:17 -0300

    feature: tela de listagem

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import { View } from 'react-native'
-import styles from './styles'
+import React from "react";
+import { View, ScrollView } from "react-native";
+import styles from "./styles";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
-function TeacherList(){
-    return <View style={styles.container}/>
+function TeacherList() {
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Proffys disponíveis" />
+            <ScrollView
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}
+                style={styles.teacherList}>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </ScrollView>
+        </View>
+    );
 }
 
-export default TeacherList
+export default TeacherList;
