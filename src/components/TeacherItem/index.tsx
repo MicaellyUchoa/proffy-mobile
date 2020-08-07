e0d22c88576bdbd50ba864ec7d1743be7b5a9e8e
MicaellyUchoa <micaellyit@hotmail.com>
2020-08-07 19:07:10 -0300

    feature: função de favoritar finalizada

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,51 +1,106 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, Linking } from "react-native";
 import styles from "./styles";
 import { RectButton } from "react-native-gesture-handler";
 import heartOutlineIcon from "../../assets/images/icons/heart-outline.png";
 import unfavoriteIcon from "../../assets/images/icons/unfavorite.png";
 import whatsappIcon from "../../assets/images/icons/whatsapp.png";
-function TeacherItem() {
+import AsyncStorage from "@react-native-community/async-storage";
+import api from "../../services/api";
+
+export interface Teacher {
+    id: number;
+    subject: string;
+    cost: number;
+    user_id: 2;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+    favorited: boolean;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+    const [isFavorites, setIsFavorites] = useState(favorited);
+    function handleLinkToWhatsapp() {
+        api.post("connections", {
+            user_id: teacher.id,
+        });
+        Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+    }
+
+    async function handleToggleFavorite() {
+        const favorites = await AsyncStorage.getItem("favorites");
+        let favoritesArray = [];
+        if (favorites) {
+            favoritesArray = JSON.parse(favorites);
+        }
+        if (isFavorites) {
+            const favoriteIndex = favoritesArray.findIndex(
+                (teacherItem: Teacher) => {
+                    return teacherItem.id === teacher.id;
+                }
+            );
+            favoritesArray.splice(favoriteIndex, 1);
+            setIsFavorites(false);
+        } else {
+            favoritesArray.push(teacher);
+
+            setIsFavorites(true);
+        }
+        await AsyncStorage.setItem("favorites", JSON.stringify(favoritesArray));
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image
                     style={styles.avatar}
                     source={{
-                        uri:
-                            "https://avatars2.githubusercontent.com/u/37215778?s=460&u=2c93abd69dae2d5b0deabd4ecae81ebdb5a3b865&v=4",
+                        uri: teacher.avatar,
                     }}
                 />
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Diego Fernandes</Text>
-                    <Text style={styles.subject}>Química</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
-            <Text style={styles.bio}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-            </Text>
+            <Text style={styles.bio}>{teacher.bio}</Text>
             <View style={styles.footer}>
                 <Text style={styles.price}>
-                    Preço/hora {"   "}{" "}
-                    <Text style={styles.priceValue}>R$20,00</Text>
+                    Preço/hora {"   "}
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
                 </Text>
                 <View style={styles.buttonsContainer}>
-                    <RectButton style={[styles.favoriteButton, styles.favorited]}>
-                        {/* <Image source={heartOutlineIcon} /> */}
-                        <Image source={unfavoriteIcon} />
+                    <RectButton
+                        onPress={handleToggleFavorite}
+                        style={[
+                            styles.favoriteButton,
+                            isFavorites ? styles.favorited : {},
+                        ]}>
+                        {isFavorites ? (
+                            <Image source={unfavoriteIcon} />
+                        ) : (
+                            <Image source={heartOutlineIcon} />
+                        )}
                     </RectButton>
 
-                    <RectButton style={styles.contactButton}>
+                    <RectButton
+                        style={styles.contactButton}
+                        onPress={handleLinkToWhatsapp}>
                         <Image source={whatsappIcon} />
-                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                        <Text style={styles.contactButtonText}>
+                            Entrar em contato
+                        </Text>
                     </RectButton>
                 </View>
             </View>
         </View>
     );
-}
+};
 
 export default TeacherItem;
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, ScrollView } from "react-native";
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
-import TeacherItem from "../../components/TeacherItem";
-
+import TeacherItem, { Teacher } from "../../components/TeacherItem";
+import AsyncStorage from "@react-native-community/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 function Favorites() {
+    const [favorites, setFavorites] = useState([]);
+    const loadFavorites = () => {
+        AsyncStorage.getItem("favorites").then(response => {
+            if (response) {
+                const favoritesTeachers = JSON.parse(response);
+
+                setFavorites(favoritesTeachers);
+            }
+        });
+    };
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
     return (
         <View style={styles.container}>
             <PageHeader title="Meus Proffys favoritos" />
@@ -14,12 +30,15 @@ function Favorites() {
                     paddingBottom: 16,
                 }}
                 style={styles.teacherList}>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited
+                        />
+                    );
+                })}
             </ScrollView>
         </View>
     );
