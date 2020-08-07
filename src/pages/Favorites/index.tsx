009cc554045e4aab30e9d3e504541e312125388e
MicaellyUchoa <micaellyit@hotmail.com>
2020-08-07 17:34:07 -0300

    feature: tela de favoritos

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import styles from "./styles";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 function Favorites() {
-    return <View style={styles.container} />;
+    return (
+        <View style={styles.container}>
+            <PageHeader title="Meus Proffys favoritos" />
+            <ScrollView
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16,
+                }}
+                style={styles.teacherList}>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </ScrollView>
+        </View>
+    );
 }
 
 export default Favorites;
